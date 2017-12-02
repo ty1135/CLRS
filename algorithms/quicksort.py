76ee43e9f85ee61577fe0a1abac2a6b5de3f5acd
goalie <ty1135@my.bristol.ac.uk>
2017-12-02 12:16:15 +0800

    Implement quicksort

---
+++ algorithms/quicksort.py
@@ -0,0 +1,32 @@
+def _swap(l, i, j):
+    temp = l[i]
+    l[i] = l[j]
+    l[j] = temp
+
+
+def quicksort(l, p, r):
+    if r - p > 0:
+        q = partition(l, p, r)
+        quicksort(l, p, q-1)
+        quicksort(l, q+1, r)# DO LEAVE 'q' THERE
+
+
+def partition(l, p, r):
+    # ensure l[p:i] <= x;
+    #        l[i:j] >= x;
+    #        l[j:r] = unrestricted
+    i = p
+    pivot = l[r]
+    for x in range(p, r):
+        if l[x] <= pivot:
+            _swap(l, i, x)
+            i += 1
+    _swap(l, i, r)
+    return i
+
+
+if __name__ == "__main__":
+    l = [3, 7, 4, 5, 11, 233, 5677, 8, 34, 56, 6]
+    print(l)
+    quicksort(l, 0, len(l)-1)
+    print(l)
